ab848456bc638ffa1a790eb57951cb6e3d3d96cb
Max <gowan_maxwell@columbusstate.edu>
2023-04-20 11:43:26 -0400

    Add project files.

---
+++ TempConversion/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tbFahrenheit = New System.Windows.Forms.TextBox()
+        Me.tbCelsius = New System.Windows.Forms.TextBox()
+        Me.lbConversion = New System.Windows.Forms.ListBox()
+        Me.rbFtoC = New System.Windows.Forms.RadioButton()
+        Me.rbCtoF = New System.Windows.Forms.RadioButton()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblConversion = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(562, 307)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(571, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(336, 31)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Temperature Conversion"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(600, 145)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Fahrenheit:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(629, 193)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Celcius:"
+        '
+        'tbFahrenheit
+        '
+        Me.tbFahrenheit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbFahrenheit.Location = New System.Drawing.Point(734, 145)
+        Me.tbFahrenheit.Name = "tbFahrenheit"
+        Me.tbFahrenheit.Size = New System.Drawing.Size(100, 26)
+        Me.tbFahrenheit.TabIndex = 4
+        '
+        'tbCelsius
+        '
+        Me.tbCelsius.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbCelsius.Location = New System.Drawing.Point(734, 193)
+        Me.tbCelsius.Name = "tbCelsius"
+        Me.tbCelsius.Size = New System.Drawing.Size(100, 26)
+        Me.tbCelsius.TabIndex = 5
+        '
+        'lbConversion
+        '
+        Me.lbConversion.FormattingEnabled = True
+        Me.lbConversion.Location = New System.Drawing.Point(577, 245)
+        Me.lbConversion.Name = "lbConversion"
+        Me.lbConversion.Size = New System.Drawing.Size(284, 95)
+        Me.lbConversion.TabIndex = 6
+        '
+        'rbFtoC
+        '
+        Me.rbFtoC.AutoSize = True
+        Me.rbFtoC.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbFtoC.Location = New System.Drawing.Point(588, 263)
+        Me.rbFtoC.Name = "rbFtoC"
+        Me.rbFtoC.Size = New System.Drawing.Size(265, 24)
+        Me.rbFtoC.TabIndex = 7
+        Me.rbFtoC.TabStop = True
+        Me.rbFtoC.Text = "Convert Fahrenheit to Celcius"
+        Me.rbFtoC.UseVisualStyleBackColor = True
+        '
+        'rbCtoF
+        '
+        Me.rbCtoF.AutoSize = True
+        Me.rbCtoF.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbCtoF.Location = New System.Drawing.Point(588, 299)
+        Me.rbCtoF.Name = "rbCtoF"
+        Me.rbCtoF.Size = New System.Drawing.Size(265, 24)
+        Me.rbCtoF.TabIndex = 8
+        Me.rbCtoF.TabStop = True
+        Me.rbCtoF.Text = "Convert Celcius to Fahrenheit"
+        Me.rbCtoF.UseVisualStyleBackColor = True
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.Location = New System.Drawing.Point(642, 363)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(88, 30)
+        Me.btnConvert.TabIndex = 9
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(746, 363)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(88, 30)
+        Me.btnClear.TabIndex = 10
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 327)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(112, 20)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Conversions:"
+        '
+        'lblConversion
+        '
+        Me.lblConversion.AutoSize = True
+        Me.lblConversion.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConversion.Location = New System.Drawing.Point(130, 327)
+        Me.lblConversion.Name = "lblConversion"
+        Me.lblConversion.Size = New System.Drawing.Size(102, 20)
+        Me.lblConversion.TabIndex = 12
+        Me.lblConversion.Text = "placeholder"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(957, 450)
+        Me.Controls.Add(Me.lblConversion)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.rbCtoF)
+        Me.Controls.Add(Me.rbFtoC)
+        Me.Controls.Add(Me.lbConversion)
+        Me.Controls.Add(Me.tbCelsius)
+        Me.Controls.Add(Me.tbFahrenheit)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents tbFahrenheit As TextBox
+    Friend WithEvents tbCelsius As TextBox
+    Friend WithEvents lbConversion As ListBox
+    Friend WithEvents rbFtoC As RadioButton
+    Friend WithEvents rbCtoF As RadioButton
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblConversion As Label
+End Class
---
+++ TempConversion/Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+    ' Initialize a queue to store the past conversion results
+    Private pastConversions As New Queue(Of String)
+    ' Set a constant to define the maximum number of history items
+    Private Const historySize As Integer = 5
+
+    ' Event handler for the "Convert" button click
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Try
+            Dim inputTemperature As Double
+            Dim convertedTemperature As Double
+            Dim resultText As String
+
+            ' Check if the "Fahrenheit to Celsius" radio button is selected
+            If rbFtoC.Checked Then
+                ' Parse the input temperature from the Fahrenheit textbox
+                inputTemperature = Double.Parse(tbFahrenheit.Text)
+                ' Perform the conversion from Fahrenheit to Celsius
+                convertedTemperature = (inputTemperature - 32) * 5 / 9
+                ' Format the result text for display
+                resultText = $"{inputTemperature} degrees in Fahrenheit is {Math.Round(convertedTemperature, 2)} degrees in Celsius"
+                ' Check if the "Celsius to Fahrenheit" radio button is selected
+            ElseIf rbCtoF.Checked Then
+                ' Parse the input temperature from the Celsius textbox
+                inputTemperature = Double.Parse(tbCelsius.Text)
+                ' Perform the conversion from Celsius to Fahrenheit
+                convertedTemperature = inputTemperature * 9 / 5 + 32
+                ' Format the result text for display
+                resultText = $"{inputTemperature} degrees in Celsius is {Math.Round(convertedTemperature, 2)} degrees in Fahrenheit"
+            Else
+                ' Display an error message if no conversion option is selected
+                MessageBox.Show("Please select a conversion option.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+            End If
+
+            ' Add the new conversion result to the queue
+            pastConversions.Enqueue(resultText)
+
+            ' Remove the oldest conversion result if the queue size exceeds the history limit
+            If pastConversions.Count > historySize Then
+                pastConversions.Dequeue()
+            End If
+
+            ' Display the conversion history in the label, separated by newlines
+            lblConversion.Text = String.Join(Environment.NewLine, pastConversions.Reverse())
+
+        Catch ex As Exception
+            ' Display an error message if the input is not valid
+            MessageBox.Show("Invalid input. Please enter a valid temperature.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Event handler for the Fahrenheit textbox text change
+    Private Sub tbFahrenheit_TextChanged(sender As Object, e As EventArgs) Handles tbFahrenheit.TextChanged
+
+    End Sub
+
+    ' Event handler for the Celsius textbox text change
+    Private Sub tbCelcius_TextChanged(sender As Object, e As EventArgs) Handles tbCelsius.TextChanged
+
+    End Sub
+
+    ' Event handler for the "Clear" button click
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear the Fahrenheit and Celsius textboxes
+        tbFahrenheit.Clear()
+        tbCelsius.Clear()
+        ' Clear the conversion history label
+        lblConversion.Text = ""
+        ' Uncheck both radio buttons
+        rbFtoC.Checked = False
+        rbCtoF.Checked = False
+        ' Clear the past conversions queue
+        pastConversions.Clear()
+    End Sub
+
+    ' Event handler for the form load event
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialize the conversion history label to an empty string
+        lblConversion.Text = ""
+    End Sub
+End Class
